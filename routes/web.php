3e5228ff9cad6fb3cf495f49a96987acce0ecff2
arislaode <laodearissaputra94@gmail.com>
2024-07-19 00:43:24 +0700

    add route index, category & admin

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/category/{category}', [HomeController::class, 'category'])->name('category');
+
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::get('/', [AdminController::class, 'index'])->name('dashboard');
+    Route::get('/news', [AdminController::class, 'manageNews'])->name('news');
+    Route::get('/categories', [AdminController::class, 'manageCategories'])->name('categories');
+    Route::get('/settings', [AdminController::class, 'settings'])->name('settings');
 });
+
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
