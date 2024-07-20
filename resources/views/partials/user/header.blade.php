9aac42d011b5edd4a2a77f8ffecf173cbb017f16
arislaode <laodearissaputra94@gmail.com>
2024-07-20 10:02:11 +0700

    update header user

--- resources/views/partials/user/header.blade.php
+++ resources/views/partials/user/header.blade.php
@@ -1,25 +1,48 @@
 <header class="bg-white shadow p-4 relative z-50">
     <div class="container mx-auto flex justify-between items-center">
-        <div class="text-lg font-bold">News App</div>
-        <nav class="hidden lg:flex space-x-4">
-            <a href="{{ url('/') }}" class="text-gray-600 hover:text-gray-800">Home</a>
-            @foreach ($categories as $category)
-                <a href="{{ url('/category/' . strtolower($category)) }}" class="text-gray-600 hover:text-gray-800">{{ $category }}</a>
-            @endforeach
-        </nav>
-        <div class="lg:hidden" x-data="{ open: false }">
-            <button @click="open = !open" class="text-gray-600 hover:text-gray-800 focus:outline-none">
-                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>
-                </svg>
-            </button>
-            <div x-show="open" @click.away="open = false" class="absolute right-0 mt-2 w-full bg-white shadow-lg rounded-lg py-2 z-50">
-                <a href="{{ url('/') }}" class="block px-4 py-2 text-gray-600 hover:text-gray-800 hover:bg-gray-100">Home</a>
-                @foreach ($categories as $category)
-                    <a href="{{ url('/category/' . strtolower($category)) }}" class="block px-4 py-2 text-gray-600 hover:text-gray-800 hover:bg-gray-100">{{ $category }}</a>
-                @endforeach
-            </div>
+        <div class="flex items-center space-x-3 rtl:space-x-reverse">
+            <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
+            <div class="text-lg font-bold">News App</div>
         </div>
+        <!-- Start of Flowbite navigation menu -->
+        <nav class="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+            <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                <button data-collapse-toggle="navbar-dropdown" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-dropdown" aria-expanded="false">
+                    <span class="sr-only">Open main menu</span>
+                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+                    </svg>
+                </button>
+                <div class="hidden w-full md:block md:w-auto" id="navbar-dropdown">
+                    <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        <li>
+                            <a href="{{ url('/') }}" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent" aria-current="page">Home</a>
+                        </li>
+                        @foreach ($categories as $category)
+                            <li>
+                                <a href="{{ route('category', ['category' => strtolower($category)]) }}" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">{{ $category }}</a>
+                            </li>
+                        @endforeach
+                        <li>
+                            <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" class="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">Regions <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+                            </svg></button>
+                            <!-- Dropdown menu -->
+                            <div id="dropdownNavbar" class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+                                <ul class="py-2 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
+                                    @foreach ($regions as $region)
+                                        <li>
+                                            <a href="{{ route('region', ['region' => strtolower($region)]) }}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{{ $region }}</a>
+                                        </li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- End of Flowbite navigation menu -->
     </div>
 </header>
 
