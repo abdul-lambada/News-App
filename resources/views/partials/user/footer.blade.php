13fd6c916091717e88f9cb2f64c676425b484a9b
arislaode <laodearissaputra94@gmail.com>
2024-07-21 00:02:51 +0700

    update footer

--- resources/views/partials/user/footer.blade.php
+++ resources/views/partials/user/footer.blade.php
@@ -1,5 +1,23 @@
-<footer class="bg-gray-800 text-white p-4 mt-4">
-    <div class="container mx-auto text-center">
-        © 2024 News App. All rights reserved.
+<footer class="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
+    <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div class="sm:flex sm:items-center sm:justify-between">
+            <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
+                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+            </a>
+            <ul class="flex flex-wrap items-center justify-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400 space-y-2 sm:space-y-0">
+                <li class="mr-4 md:mr-6">
+                    <a href="#" class="hover:underline">About</a>
+                </li>
+                <li class="mr-4 md:mr-6">
+                    <a href="#" class="hover:underline">Privacy Policy</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline">Contact</a>
+                </li>
+            </ul>
+        </div>
+        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span class="block text-sm text-gray-500 text-center dark:text-gray-400 mt-4">© 2024 <a href="#" class="hover:underline">News Talenthub</a>. All Rights Reserved.</span>
     </div>
 </footer>
