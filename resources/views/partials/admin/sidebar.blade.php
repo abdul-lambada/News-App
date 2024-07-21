978639f72d14d7554e22d82054a029fbefd2a91a
arislaode <laodearissaputra94@gmail.com>
2024-07-21 20:54:27 +0700

    update sidebar

--- resources/views/partials/admin/sidebar.blade.php
+++ resources/views/partials/admin/sidebar.blade.php
@@ -1,56 +1,49 @@
-<aside :class="{'translate-x-0': sidebarOpen, '-translate-x-full': !sidebarOpen}" class="fixed z-30 inset-y-0 left-0 w-64 bg-gray-800 text-white transform transition duration-200 ease-in-out lg:translate-x-0 lg:static lg:inset-0 h-full">
-    <div class="p-6 relative">
-        <button @click="sidebarOpen = !sidebarOpen" class="text-white hover:text-gray-300 focus:outline-none absolute top-4 -right-8 transform translate-x-1/2 bg-gray-800 p-2 rounded-full lg:hidden">
-            <svg x-show="!sidebarOpen" class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>
-            </svg>
-            <svg x-show="sidebarOpen" class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>
-            </svg>
-        </button>
-        <nav>
-            <ul>
-                <li class="mb-4">
-                    <a href="{{ route('admin.dashboard') }}" class="flex items-center text-gray-300 hover:text-white">
-                        <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M13 5v6a4 4 0 00-4 4h7l1-1 2 2-9 9-9-9 1-1h6a4 4 0 004-4V5m0 0l-7-7-7 7m0 0L5 12m0 0l-2-2"></path>
-                        </svg>
-                        Dashboard
-                    </a>
-                </li>
-                <li class="mb-4">
-                    <a href="{{ route('admin.news') }}" class="flex items-center text-gray-300 hover:text-white">
-                        <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 7h18M3 12h18M3 17h18"></path>
-                        </svg>
-                        Manage News
-                    </a>
-                </li>
-                <li class="mb-4">
-                    <a href="{{ route('admin.categories') }}" class="flex items-center text-gray-300 hover:text-white">
-                        <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 6h-6v6h6V6zM4 14h6v6H4v-6zM4 6h6v6H4V6zM14 14h6v6h-6v-6z"></path>
-                        </svg>
-                        Manage Categories
-                    </a>
-                </li>
-                <li class="mb-4">
-                    <a href="{{ route('admin.settings') }}" class="flex items-center text-gray-300 hover:text-white">
-                        <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11.992 2c-.784 0-1.45.663-1.45 1.454v1.492c-1.152.226-2.25.713-3.236 1.443l-1.052-1.053c-.567-.567-1.486-.567-2.052 0-.566.566-.566 1.486 0 2.053l1.053 1.052c-.73.986-1.216 2.084-1.442 3.236H2.454C1.663 11.993 1 12.66 1 13.444c0 .784.663 1.451 1.454 1.451h1.492c.226 1.152.713 2.25 1.443 3.236l-1.053 1.052c-.567.567-.567 1.486 0 2.052.566.566 1.486.566 2.053 0l1.052-1.053c.986.73 2.084 1.216 3.236 1.442v1.492c0 .784.666 1.454 1.451 1.454.784 0 1.451-.67 1.451-1.454v-1.492c1.152-.226 2.25-.713 3.236-1.443l1.052 1.053c.567.567 1.486.567 2.053 0 .566-.566.566-1.486 0-2.052l-1.053-1.052c.73-.986 1.216-2.084 1.442-3.236h1.492c.784 0 1.454-.667 1.454-1.451 0-.784-.67-1.451-1.454-1.451h-1.492c-.226-1.152-.713-2.25-1.443-3.236l1.053-1.052c.566-.566.566-1.486 0-2.053-.567-.567-1.486-.567-2.052 0l-1.053 1.053c-.986-.73-2.084-1.216-3.236-1.442V3.454C13.443 2.663 12.775 2 11.992 2zM12 8.6c-1.88 0-3.4 1.521-3.4 3.4 0 1.879 1.521 3.4 3.4 3.4 1.879 0 3.4-1.521 3.4-3.4 0-1.879-1.521-3.4-3.4-3.4z"></path>
-                        </svg>
-                        Settings
-                    </a>
-                </li>
-                <li class="lg:hidden mb-4">
-                    <a href="{{ route('logout') }}" class="flex items-center text-gray-300 hover:text-white">
-                        <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1m0-10V5a2 2 0 00-2-2H7a2 2 0 00-2 2v14a2 2 0 002 2h6a2 2 0 002-2v-1"></path>
-                        </svg>
-                        Logout
-                    </a>
-                </li>
-            </ul>
-        </nav>
+<aside :class="{ '-translate-x-full': !sidebarOpen, 'translate-x-0': sidebarOpen }" class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
+    <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
+        <ul class="space-y-2 font-medium">
+            <li>
+                <a href="{{ route('admin.dashboard') }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                    <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
+                        <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
+                        <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"/>
+                    </svg>
+                    <span class="ms-3">Dashboard</span>
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('admin.news') }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                    <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
+                        <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z"/>
+                    </svg>
+                    <span class="flex-1 ms-3 whitespace-nowrap">Berita</span>
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('admin.categories') }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                    <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
+                        <path d="M4 4h3V1H4v3zM4 10h3V7H4v3zM4 16h3v-3H4v3zM10 4h3V1h-3v3zM10 10h3V7h-3v3zM10 16h3v-3h-3v3zM16 4h3V1h-3v3zM16 10h3V7h-3v3zM16 16h3v-3h-3v3z"/>
+                    </svg>
+                    <span class="flex-1 ms-3 whitespace-nowrap">Kategori</span>
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('admin.settings') }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                    <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                        <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.96 2.96 0 0 0 .13 5H5Z"/>
+                        <path d="M6.737 11.061a2.961 2.961 0 0 1 .81-1.515l6.117-6.116A4.839 4.839 0 0 1 16 2.141V2a1.97 1.97 0 0 0-1.933-2H7v5a2 2 0 0 1-2 2H0v11a1.969 1.969 0 0 0 1.933 2h12.134A1.97 1.97 0 0 0 16 18v-3.093l-1.546 1.546c-.413.413-.94.695-1.513.81l-3.4.679a2.947 2.947 0 0 1-1.85-.227 2.96 2.96 0 0 1-1.635-3.257l.681-3.397Z"/>
+                        <path d="M8.961 16a.93.93 0 0 0 .189-.019l3.4-.679a.961.961 0 0 0 .49-.263l6.118-6.117a2.884 2.884 0 0 0-4.079-4.078l-6.117 6.117a.96.96 0 0 0-.263.491l-.679 3.4A.961.961 0 0 0 8.961 16Zm7.477-9.8a.958.958 0 0 1 .68-.281.961.961 0 0 1 .682 1.644l-.315.315-1.36-1.36.313-.318Zm-5.911 5.911 4.236-4.236 1.359 1.359-4.236 4.237-1.7.339.341-1.699Z"/>
+                    </svg>
+                    <span class="flex-1 ms-3 whitespace-nowrap">Settings</span>
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('logout') }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                    <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1m0-10V5a2 2 0 00-2-2H7a2 2 0 00-2 2v14a2 2 0 002 2h6a2 2 0 002-2v-1" />
+                    </svg>
+                    <span class="flex-1 ms-3 whitespace-nowrap">Logout</span>
+                </a>
+            </li>
+        </ul>
     </div>
 </aside>
