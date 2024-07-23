54667a9203adeed90393ba0b0b4bd6249135884a
Abdul Kholik <engineertekno@gmail.com>
2024-07-23 22:08:39 +0700

    Update Providers:

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,38 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+// use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use Laravel\Passport\Passport;
 
-class AppServiceProvider extends ServiceProvider
+class AuthServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
      */
-    public function register(): void
-    {
-        //
-    }
+    // public function register(): void
+    // {
+    //     //
+    // }
+
+    protected $policies = [
+        // 'App/Models/Model' => 'App\Polices\ModelPolicy',
+    ];
 
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    // public function boot(): void
+    // {
+    //     //
+    // }
+
+    public function boot()
     {
-        //
+        $this->registerPolicies();
+
+        //Laravel Passport Routes
+        Passport::routes();
     }
 }
