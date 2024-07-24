fb170f220856de0485aebfee8efd115bca64efaa
Abdul Kholik <engineertekno@gmail.com>
2024-07-24 10:06:37 +0700

    update Role Model

---
+++ app/Models/Rule.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Rule extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+    ];
+
+    public function users(){
+            // return $this->belongsToMany(User::class);
+            return $this->hasMany(User::class);
+    }
+}
