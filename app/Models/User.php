de1f50e8efc5aadef0f71b583a298214f3c75b31
Abdul Kholik <engineertekno@gmail.com>
2024-07-24 11:59:55 +0700

    Update User

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -20,6 +21,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'phone_number',
+        'address',
+        'profile+photo',
+        'role_id', // Penambahan rore_id fo fillable attributed
     ];
 
     /**
@@ -37,11 +42,12 @@ class User extends Authenticatable
      *
      * @return array<string, string>
      */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+    protected $casts = [
+        'email_verified_at' => 'datatime',
+    ];
+
+    public function role(){
+        // return $this->belongToMany(Role::class);
+        return $this->belongsTo(Role::class);
     }
 }
