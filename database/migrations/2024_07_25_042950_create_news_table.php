9357ba1d4295e4952c42d6b6ef8bda81bede7727
Abdul Kholik <engineertekno@gmail.com>
2024-07-25 11:30:35 +0700

    Create table migrations News

---
+++ database/migrations/2024_07_25_042950_create_news_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('body');
+            $table->foreignId('category_id')->constrained('categories');
+            $table->foreignId('region_id')->constrained('regions');
+            $table->string('image_url');
+            $table->string('image_caption');
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('news');
+    }
+};
