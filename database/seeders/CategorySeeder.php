3bfd1e3acdf162b9873e463d70aaa1e32e5d7d2e
Abdul Kholik <engineertekno@gmail.com>
2024-07-25 18:20:33 +0700

    Update seeder category

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -10,6 +10,7 @@ class CategorySeeder extends Seeder
     public function run()
     {
         $categories = ['Politik', 'Kesehatan', 'Teknologi', 'Hiburan', 'Olahraga'];
+        $categories = ['Politik', 'Kesehatan', 'Teknologi', 'Hiburan', 'Olahraga'. 'opini', 'komunitas', 'edukasi'];
 
         foreach ($categories as $category) {
             Category::create(['name' => $category]);
